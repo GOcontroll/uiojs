049521047759ce6fca5a7fdf00604fd4bad2b6e1
SpieringsAE <ben_spierings@hotmail.com>
2022-12-02 09:39:14 +0100

    fixed uint8/int8 processing, tested

--- README.md
+++ README.md
@@ -3,3 +3,110 @@ A node addon for making Userspace IO on Linux accessible by javascript.
 
 Currently implements process_vm_readv and process_vm_writev from sys/uio. \
 https://man7.org/linux/man-pages/man2/process_vm_readv.2.html
+
+# testing
+
+due to the nature of this addon it is beyond my capabilities to automatically test it, but it does work, at least on my arm v8 system (IMX8MM) running kernel 5.15.32 and debian 11.
+
+int64 and uint64 have not been tested yet, I have no use for them at this point, but if one experiences problems with them head over to the issues tab: \
+https://github.com/GOcontroll/uiojs/issues
+
+test results:
+
+testing doubles
+
+read current double = [ 0 ]
+write 2500.2851
+read current double = [ 2500.2851 ]
+write 0
+read current double = [ 0 ]
+
+read current double array = [ 0, 0, 0 ]
+write [ 2500.2851, 10.6, -1000.684 ]
+read current double array = [ 2500.2851, 10.6, -1000.684 ]
+write [ 0, 0, 0 ]
+read current double array = [ 0, 0, 0 ]
+
+testing singles
+
+read current single = [ 0 ]
+write 2500.2851
+read current single = [ 2500.28515625 ]
+write 0
+read current single = [ 0 ]
+
+read current single array = [ 0, 0, 0 ]
+write [ 2500.2851, 10.6, -1000.684]
+read current single array = [ 2500.28515625, 10.600000381469727, -1000.6840209960938 ]
+write [ 0, 0, 0 ]
+read current single array = [ 0, 0, 0 ]
+
+testing int32
+
+read current value = [ 0 ]
+write 2500000
+read current value = [ 2500000 ]
+write  -600000
+read current value = [ -600000 ]
+write 0
+read current value = [ 0 ]
+
+read arr = [ 0, 0, 0 ]
+write [ 2500000, 10, -1000000 ]
+read arr = [ 2500000, 10, -1000000 ]
+write [ 0, 0, 0 ]
+read arr = [ 0, 0, 0 ]
+
+and so on... see test/test_binding.js for the test program
+
+testing uint32
+
+[ 0 ]
+[ 2500000 ]
+[ 0 ]
+
+[ 0, 0, 0 ]
+[ 2500000, 10, 1000000 ]
+[ 0, 0, 0 ]
+
+testing int16
+
+[ 0 ]
+[ 25000 ]
+[ -6000 ]
+[ 0 ]
+
+[ 0, 0, 0 ]
+[ 25000, 10, -10000 ]
+[ 0, 0, 0 ]
+
+testing uint16
+
+[ 0 ]
+[ 60000 ]
+[ 0 ]
+
+[ 0, 0, 0 ]
+[ 25000, 10, 45000 ]
+[ 0, 0, 0 ]
+
+testing int8
+
+[ 0 ]
+[ 100 ]
+[ -98 ]
+[ 0 ]
+
+[ 0, 0, 0 ]
+[ 20, 10, -100 ]
+[ 0, 0, 0 ]
+
+testing uint8
+
+[ 0 ]
+[ 200 ]
+[ 0 ]
+
+[ 0, 0, 0 ]
+[ 250, 10, 45 ]
+[ 0, 0, 0 ]
--- test/test_binding.js
+++ test/test_binding.js
@@ -1,23 +1,138 @@
-const Uiojs = require("../lib/binding.js");
-const assert = require("assert");
+const uiojs = require("./lib/binding.js")
 
-assert(Uiojs, "The expected function is undefined");
+let doublePar = new uiojs.asap_element(0x422558, uiojs.dataTypes.double, 1);
+let doubleArr = new uiojs.asap_element(0x422540, uiojs.dataTypes.double, 3);
+let singlePar = new uiojs.asap_element(0x422594, uiojs.dataTypes.single, 1);
+let singleArr = new uiojs.asap_element(0x422588, uiojs.dataTypes.single, 3);
+let int32Par  = new uiojs.asap_element(0x42257c, uiojs.dataTypes.int32,  1);
+let int32Arr  = new uiojs.asap_element(0x422570, uiojs.dataTypes.int32,  3);
+let uint32Par = new uiojs.asap_element(0x4225ac, uiojs.dataTypes.uint32, 1);
+let uint32Arr = new uiojs.asap_element(0x4225a0, uiojs.dataTypes.uint32, 3);
+let int16Par  = new uiojs.asap_element(0x42256e, uiojs.dataTypes.int16,  1);
+let int16Arr  = new uiojs.asap_element(0x422568, uiojs.dataTypes.int16,  3);
+let uint16Par = new uiojs.asap_element(0x42259e, uiojs.dataTypes.uint16, 1);
+let uint16Arr = new uiojs.asap_element(0x422598, uiojs.dataTypes.uint16, 3);
+let int8Par   = new uiojs.asap_element(0x422583, uiojs.dataTypes.int8,   1);
+let int8Arr   = new uiojs.asap_element(0x422580, uiojs.dataTypes.int8,   3);
+let uint8Par  = new uiojs.asap_element(0x4225b3, uiojs.dataTypes.uint8,  1);
+let uint8Arr  = new uiojs.asap_element(0x4225b0, uiojs.dataTypes.uint8,  3);
 
-let dutycyle = new Uiojs.asap_element(0x422540, Uiojs.dataTypes.uint16, 1);
+let pid = 2131;
 
-console.log(Uiojs.dataTypes.uint16);
-console.log(dutycyle.size_t);
+console.log("testing doubles\n");
 
-let pid = 2088;
+console.log(uiojs.process_read(pid, doublePar));
+uiojs.process_write(pid, doublePar, 2500.2851);
+console.log(uiojs.process_read(pid, doublePar));
+uiojs.process_write(pid, doublePar, 0);
+console.log(uiojs.process_read(pid, doublePar));
 
-function testBasic()
-{
-    var result = Uiojs.process_read(pid, dutycyle)
-    Uiojs.process_write(pid, dutycyle, 50);
-    result = Uiojs.process_read(pid, dutycyle)
-    Uiojs.process_write(pid, dutycyle, 0);
-}
+console.log(uiojs.process_read(pid, doubleArr));
+uiojs.process_write(pid, doubleArr, [2500.2851, 10.6, -1000.684]);
+console.log(uiojs.process_read(pid, doubleArr));
+uiojs.process_write(pid, doubleArr, [0,0,0]);
+console.log(uiojs.process_read(pid, doubleArr));
 
-assert.doesNotThrow(testBasic, undefined, "testBasic threw an expection");
+console.log("testing singles\n");
 
-console.log("Tests passed- everything looks OK!");
+console.log(uiojs.process_read(pid, singlePar));
+uiojs.process_write(pid, singlePar, 2500.2851);
+console.log(uiojs.process_read(pid, singlePar));
+uiojs.process_write(pid, singlePar, 0);
+console.log(uiojs.process_read(pid, singlePar));
+
+console.log(uiojs.process_read(pid, singleArr));
+uiojs.process_write(pid, singleArr, [2500.2851, 10.6, -1000.684]);
+console.log(uiojs.process_read(pid, singleArr));
+uiojs.process_write(pid, singleArr, [0,0,0]);
+console.log(uiojs.process_read(pid, singleArr));
+
+console.log("testing int32\n");
+
+console.log(uiojs.process_read(pid, int32Par));
+uiojs.process_write(pid, int32Par, 2500000);
+console.log(uiojs.process_read(pid, int32Par));
+uiojs.process_write(pid, int32Par, -600000);
+console.log(uiojs.process_read(pid, int32Par));
+uiojs.process_write(pid, int32Par, 0);
+console.log(uiojs.process_read(pid, int32Par));
+
+console.log(uiojs.process_read(pid, int32Arr));
+uiojs.process_write(pid, int32Arr, [2500000, 10, -1000000]);
+console.log(uiojs.process_read(pid, int32Arr));
+uiojs.process_write(pid, int32Arr, [0,0,0]);
+console.log(uiojs.process_read(pid, int32Arr));
+
+console.log("testing uint32\n");
+
+console.log(uiojs.process_read(pid, uint32Par));
+uiojs.process_write(pid, uint32Par, 2500000);
+console.log(uiojs.process_read(pid, uint32Par));
+uiojs.process_write(pid, uint32Par, 0);
+console.log(uiojs.process_read(pid, uint32Par));
+
+console.log(uiojs.process_read(pid, uint32Arr));
+uiojs.process_write(pid, uint32Arr, [2500000, 10, 1000000]);
+console.log(uiojs.process_read(pid, uint32Arr));
+uiojs.process_write(pid, uint32Arr, [0,0,0]);
+console.log(uiojs.process_read(pid, uint32Arr));
+
+console.log("testing int16\n");
+
+console.log(uiojs.process_read(pid, int16Par));
+uiojs.process_write(pid, int16Par, 25000);
+console.log(uiojs.process_read(pid, int16Par));
+uiojs.process_write(pid, int16Par, -6000);
+console.log(uiojs.process_read(pid, int16Par));
+uiojs.process_write(pid, int16Par, 0);
+console.log(uiojs.process_read(pid, int16Par));
+
+console.log(uiojs.process_read(pid, int16Arr));
+uiojs.process_write(pid, int16Arr, [25000, 10, -10000]);
+console.log(uiojs.process_read(pid, int16Arr));
+uiojs.process_write(pid, int16Arr, [0,0,0]);
+console.log(uiojs.process_read(pid, int16Arr));
+
+console.log("testing uint16\n");
+
+console.log(uiojs.process_read(pid, uint16Par));
+uiojs.process_write(pid, uint16Par, 60000);
+console.log(uiojs.process_read(pid, uint16Par));
+uiojs.process_write(pid, uint16Par, 0);
+console.log(uiojs.process_read(pid, uint16Par));
+
+console.log(uiojs.process_read(pid, uint16Arr));
+uiojs.process_write(pid, uint16Arr, [25000, 10, 45000]);
+console.log(uiojs.process_read(pid, uint16Arr));
+uiojs.process_write(pid, uint16Arr, [0,0,0]);
+console.log(uiojs.process_read(pid, uint16Arr));
+
+console.log("testing int8\n");
+
+console.log(uiojs.process_read(pid, int8Par));
+uiojs.process_write(pid, int8Par, 100);
+console.log(uiojs.process_read(pid, int8Par));
+uiojs.process_write(pid, int8Par, -98);
+console.log(uiojs.process_read(pid, int8Par));
+uiojs.process_write(pid, int8Par, 0);
+console.log(uiojs.process_read(pid, int8Par));
+
+console.log(uiojs.process_read(pid, int8Arr));
+uiojs.process_write(pid, int8Arr, [20, 10, -100]);
+console.log(uiojs.process_read(pid, int8Arr));
+uiojs.process_write(pid, int8Arr, [0,0,0]);
+console.log(uiojs.process_read(pid, int8Arr));
+
+console.log("testing uint8\n");
+
+console.log(uiojs.process_read(pid, uint8Par));
+uiojs.process_write(pid, uint8Par, 200);
+console.log(uiojs.process_read(pid, uint8Par));
+uiojs.process_write(pid, uint8Par, 0);
+console.log(uiojs.process_read(pid, uint8Par));
+
+console.log(uiojs.process_read(pid, uint8Arr));
+uiojs.process_write(pid, uint8Arr, [250, 10, 45]);
+console.log(uiojs.process_read(pid, uint8Arr));
+uiojs.process_write(pid, uint8Arr, [0,0,0]);
+console.log(uiojs.process_read(pid, uint8Arr));
